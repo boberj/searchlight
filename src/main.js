a72bfee4ad3d99370153f78914528c97115ebbfa
Johan Boberg <jboberg@gmail.com>
2017-10-25 23:29:08 -0400

    Add vuex

--- src/main.js
+++ src/main.js
@@ -1,16 +1,24 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import Vuex from 'vuex'
 import VueMoment from 'vue-moment'
 import App from './App'
 
 Vue.config.productionTip = false
 
+Vue.use(Vuex)
 Vue.use(VueMoment)
+
+const store = new Vuex.Store({
+  state: {},
+  mutations: {}
+})
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  store,
   template: '<App/>',
   components: { App }
 })
