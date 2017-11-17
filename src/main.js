f7d6bb003ba656cd1fd143f9d5e1506a894abe7d
Johan Boberg <jboberg@gmail.com>
2017-11-16 20:27:19 -0500

    Move store to separate file

--- src/main.js
+++ src/main.js
@@ -6,12 +6,9 @@ import VueMoment from 'vue-moment'
 import App from './App'
 import auth from '@/lib/auth'
 import Database from '@/lib/database'
-import Playlist from '@/repositories/playlist'
-import Playlists from '@/services/playlists'
-import * as R from 'ramda'
-import Search from '@/services/search'
 import spotify from '@/lib/spotify'
 import { State } from '@/constants'
+import Store from '@/store'
 
 Vue.config.productionTip = false
 
@@ -29,95 +26,7 @@ if (auth.loggedIn()) {
   initialState = State.UNAUTHENTICATED
 }
 
-const store = new Vuex.Store({
-  state: {
-    progress: 0,
-    state: initialState,
-    tracks: [],
-    index: null
-  },
-  mutations: {
-    syncing (state) {
-      state.state = State.SYNCING
-    },
-    indexing (state) {
-      state.state = State.INDEXING
-    },
-    ready (state) {
-      state.state = State.READY
-    },
-    progress (state, progress) {
-      state.progress = progress
-    },
-    tracks (state, tracks) {
-      state.tracks = tracks
-    },
-    index (state, index) {
-      state.index = index
-    }
-  },
-  actions: {
-    sync ({ commit, dispatch }) {
-      commit('syncing')
-      return Playlists.syncPlaylists(
-        db, spotify, (progress) => { commit('progress', progress) }
-      ).then(() => {
-        commit('indexing')
-        return dispatch('loadPlaylists')
-      }).then(() => {
-        commit('ready')
-      })
-    },
-    loadPlaylists ({ commit, dispatch }) {
-      return Playlist.findAll(db)
-        .then((playlists) => {
-          return dispatch('transformPlaylists', playlists)
-        }).then((tracks) => {
-          commit('tracks', Object.freeze(tracks))
-          commit('progress', 0)
-          return Search.createIndex(tracks, (progress) => { commit('progress', progress) })
-        }).then((index) => {
-          commit('index', Object.freeze(index))
-        })
-    },
-    /**
-     * Tranforms
-     *
-     * From (document)                 To (table)
-     *
-     * [{                              [{
-     *   id                              playlist {
-     *   name                              name
-     *   snapshot_id                     }
-     *   owner {                         track {
-     *     id                              album {
-     *   }                                   name
-     *   tracks [{                         }
-     *     added_at                        artists [{
-     *     track {                           name
-     *       album {                       }]
-     *         name                        duration_ms
-     *       }                             name
-     *       artists [{                  }
-     *         name                      added_at
-     *       }]                        }]
-     *       duration_ms
-     *       name
-     *     }
-     *   }]
-     * }]
-     */
-    transformPlaylists (context, playlists) {
-      return R.chain(
-        playlist => R.map(
-          R.assoc('playlist', { name: playlist.name }),
-          R.filter(track => track.track !== null, playlist.tracks)
-        ),
-        playlists
-      )
-    }
-  }
-})
+const store = Store.createStore(initialState, db, spotify)
 
 /* eslint-disable no-new */
 new Vue({
